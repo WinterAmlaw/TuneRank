1f134a2a0054fbf60eacf6638f502a3446095f18
Winter <winteramlaw@gmail.com>
2023-06-02 14:36:50 -0400

    navbar styles update and responsive layout, logo design

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,65 +1,182 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
+
 const Navbar = () => {
+  const location = useLocation();
+  if (location.pathname === '/signup' || location.pathname === '/login') {
+    return null; 
+  }
   return (
-    <Nav>
-      <Logo to="/">
-        TuneRank
+    <NavContainer>
+      <Logo href="/">
+        <span>Tune</span>Rank
       </Logo>
-      <Menu>
-        <NavItem to="/signup">Signup</NavItem>
-        <NavItem to="/login">Login</NavItem>
-      </Menu>
-    </Nav>
+      <SearchContainer>
+        <SearchBar type="text" placeholder="Search..." />
+        <SearchButton>Go</SearchButton>
+      </SearchContainer>
+      <RightContainer>
+        <NavItem to="/signup">Sign Up</NavItem>
+        <NavItem to="/login">Log In</NavItem>
+      </RightContainer>
+    </NavContainer>
   );
 };
 
 export default Navbar;
 
 // Styles
-const Nav = styled.nav`
+const NavContainer = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
-  color: #fff;
-  padding: 10px;
-  border-bottom: 3px solid #f60;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+  @media (max-width: 992px) {
+    padding: 10px;
+  }
+
+  @media (max-width: 768px) {
+    // flex-wrap: wrap-reverse;
+    flex-direction: column;
+  }
 `;
 
-const Logo = styled(Link)`
-  color: #fff;
+const Logo = styled.a`
+  display: inline-flex;
+  align-items: center;
+  color: #000;
   text-decoration: none;
   font-size: 32px;
   font-weight: bold;
   letter-spacing: 2px;
-  text-shadow: 2px 2px 5px #000;
+
+  span {
+    background-color: #f60;
+    padding: 5px;
+    border-radius: 5px;
+    margin-right: 10px;
+    color: #fff;
+    transform: skew(-20deg);
+  }
+
+  @media (max-width: 992px) {
+    font-size: 24px;
+  }
+
+  @media (max-width: 768px) {
+    // order: 3;
+    margin-top: 10px;
+    margin-bottom: 24px;
+  }
 `;
 
-const Menu = styled.ul`
+const SearchContainer = styled.div`
+  display: inline-block;
+  margin-left: 50px;
+  margin-right: auto;
+
+  @media (max-width: 992px) {
+    margin-left: 10px;
+    width: 50%;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-left: 0;
+    margin-bottom: 10px;
+  }
+`;
+
+const SearchBar = styled.input`
+  height: 32px;
+  width: 400px;
+  padding: 5px;
+  font-size: 16px;
+  border: 2px solid #ccc;
+  background: #f7f7f7;
+  color: #444;
+  border-radius: 10px 0px 0px 10px;
+
+  @media (max-width: 1200px) {
+    width: 300px;
+  }
+
+  @media (max-width: 992px) {
+    width: calc(100% - 80px);
+  }
+
+  @media (max-width: 768px) {
+    width: calc(100% - 80px);
+  }
+
+  &:focus {
+    // border-color: #f60;
+    // box-shadow: 0 0 0 0 #f60;
+  }
+`;
+
+const SearchButton = styled.button`
+  height: 46px;
+  width: 60px;
+  border: none;
+  background: #f60;
+  color: #fff;
+  font-size: 16px;
+  border-radius: 0px 10px 10px 0px;
+  cursor: pointer;
+  margin-left: -2px;
+
+
+  @media (max-width: 768px) {
+    margin-bottom: 10px;
+  }
+
+  &:hover {
+    background-color: #f90;
+  }
+}`;
+
+const RightContainer = styled.div`
   display: flex;
-  list-style: none;
-  margin: 0;
-  padding: 0;
+  justify-content: flex-end;
+
+  @media (max-width: 992px) {
+    width: 50%;
+  }
+
+  @media (max-width: 768px) {
+    order: 2;
+    width: 100%;
+    justify-content: space-around;
+    margin-top: 10px;
+  }
 `;
 
 const NavItem = styled(Link)`
-  color: #fff;
+  display: inline-block;
+  padding: 10px 15px;
+  margin: 0 5px;
+  font-size: 18px;
+  color: #000;
   text-decoration: none;
-  margin: 0 20px;
-  font-size: 20px;
-  font-weight: bold;
-  text-shadow: 2px 2px 5px #000;
-  
+  text-transform: uppercase;
+
   &:hover {
     color: #f60;
   }
-  
-  &:active,
-  &:focus {
-    outline: none;
-    text-decoration: underline;
+
+  @media (max-width: 992px) {
+    padding: 10px;
+    margin-right: 10px;    
   }
-`;
+
+  @media (max-width: 768px) {
+    display: block;
+    margin: 0;
+  }
+}`;
