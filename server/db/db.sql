c2115b94acc27ce4b060bdf18e4e44eed8af1afe
Winter <winteramlaw@gmail.com>
2023-06-05 09:03:25 -0400

    added user profiles schema to db.sql

--- server/db/db.sql
+++ server/db/db.sql
@@ -67,3 +67,40 @@ SELECT * FROM songs WHERE album_id = <album_id>;
 -- And to retrieve all ratings for a particular album and song, you can use a query like this:
 SELECT * FROM song_ratings WHERE album_id = <album_id> AND song_id = <song_id>;
 
+
+CREATE TABLE profiles (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  artist_id INTEGER REFERENCES artists(id),
+  album_id INTEGER REFERENCES albums(id),
+  song_id INTEGER REFERENCES songs(id)
+);
+
+-- Insert a new artist and link it to the user's profile
+INSERT INTO artists (name, genre, cover_image)
+VALUES ('Pink Floyd', 'Progressive rock', 'pink-floyd.jpg');
+
+INSERT INTO profiles (user_id, artist_id)
+VALUES (2, 1);
+
+-- Insert a new album and link it to the user's profile
+INSERT INTO albums (title, artist_id, release_date, genre, cover_image, added_by)
+VALUES ('Dark Side of the Moon', 1, '1973-03-01', 'Progressive rock', 'dark-side.jpg', 2);
+
+INSERT INTO profiles (user_id, album_id)
+VALUES (2, 1);
+
+-- Insert a new song and link it to the user's profile
+INSERT INTO songs (title, track_number, album_id)
+VALUES ('Money', 6, 1);
+
+INSERT INTO profiles (user_id, song_id, album_id)
+VALUES (2, 1, 1);
+
+-- To display the name of the user who added an artist, album, or song on the frontend, you can join the profiles table with the users table using the user_id column. For example, to retrieve the details of an artist and display the name of the user who added it, you can use a query like this:
+
+SELECT artists.*, users.username AS added_by
+FROM profiles 
+JOIN artists ON profiles.artist_id = artists.id
+JOIN users ON profiles.user_id = users.id
+WHERE artists.id = 1; -- replace with the ID of the artist you want to retrieve
