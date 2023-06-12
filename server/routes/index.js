7f9d8f134bbf60cce357adb693f6f2226075d606
Winter <winteramlaw@gmail.com>
2023-06-12 00:10:15 -0400

    debugged errors on album router

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const userController = require('../controllers/userController');
 const artistsController = require('../controllers/artistsController');
+const albumsController = require('../controllers/albumsController');
 
 
 // USER ROUTES
@@ -31,15 +32,15 @@ router.delete('/artists/:id', authMiddleware.authorizeUser, artistsController.de
 
 // ALBUM ROUTES
 //post album
-router.post('/albums', authMiddleware.authorizeUser, albumsController.postNewAlbums);
+router.post('/albums', authMiddleware.authorizeUser, albumsController.postNewAlbum);
 //get all
 router.get('/albums', albumsController.getAllAlbums);
 //get by id
-router.get('/albums/:id', albumsController.getAlbumsById);
+router.get('/albums/:id', albumsController.getAlbumById);
 //update by id 
-router.put('/albums/:id', authMiddleware.authorizeUser, albumsController.updateAlbumsById);
+router.put('/albums/:id', authMiddleware.authorizeUser, albumsController.updateAlbumById);
 //delete Albums by id
-router.delete('/albums/:id', authMiddleware.authorizeUser, albumsController.deleteAlbumsById);
+router.delete('/albums/:id', authMiddleware.authorizeUser, albumsController.deleteAlbumById);
 
 //STATIC ROUTES
 
