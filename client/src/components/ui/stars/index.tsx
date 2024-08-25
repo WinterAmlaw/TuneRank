5c1e6de2e120939d28e93c552e56c05e41d2f345
WinterAmlaw <winteramlaw@gmail.com>
2024-08-25 15:33:16 -0400

    feat: updated stars component to allow user selection

--- client/src/components/ui/stars/index.tsx
+++ client/src/components/ui/stars/index.tsx
@@ -1,31 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-import { StarsProps } from '../../../interfaces/ui'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+import { StarsProps } from '../../../interfaces/ui';
 
+interface InteractiveStarsProps extends StarsProps {
+  onRatingChange?: (rating: number) => void;
+}
 
-const Stars : React.FC<StarsProps> = ({ stars, reviews }) => {
+const Stars: React.FC<InteractiveStarsProps> = ({ stars, reviews, onRatingChange }) => {
+  const [hoveredStars, setHoveredStars] = useState<number>(0);
+  const [selectedStars, setSelectedStars] = useState<number>(stars);
+
   const tempStars = Array.from({ length: 5 }, (_, index) => {
-    const number = index + 0.5
+    const number = index + 1;
     return (
-      <span key={ index }>
-        {stars > number ? (
+      <span
+        key={index}
+        onMouseEnter={() => setHoveredStars(number)}
+        onMouseLeave={() => setHoveredStars(0)}
+        onClick={() => handleRating(number)}
+      >
+        {hoveredStars >= number || (selectedStars >= number && hoveredStars === 0) ? (
           <BsStarFill />
-        ) : stars > index ? (
+        ) : (selectedStars >= index + 0.5 && hoveredStars === 0) ? (
           <BsStarHalf />
         ) : (
           <BsStar />
         )}
       </span>
-    )
-  })
+    );
+  });
+
+  const handleRating = (rating: number) => {
+    setSelectedStars(rating);
+    if (onRatingChange) {
+      onRatingChange(rating);
+    }
+  };
+
   return (
     <Wrapper>
-      <div className='stars'>{ tempStars }</div>
-      <p className='reviews'>({ reviews } customer reviews)</p>
+      <div className='stars'>{tempStars}</div>
+      <p className='reviews'>({reviews} customer reviews)</p>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -34,6 +53,11 @@ const Wrapper = styled.div`
     color: #ffb900;
     font-size: 1rem;
     margin-right: 0.25rem;
+    cursor: pointer; 
+    transition: transform 0.2s ease-in-out;
+  }
+  span:hover {
+    transform: scale(1.2); 
   }
   p {
     margin-left: 0.5rem;
